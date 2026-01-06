7e1fe16fd6c347d31e950cf1a6ac28cd8b46dc45
Tranku <148291727+Tranku28@users.noreply.github.com>
2026-01-06 01:42:44 +0100

    Console

---
+++ SRD-5-2-1 Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using SRD5_2_1.Core;
+
+bool found = false;
+for (int i = 0; !found; i++)
+{
+    if (Roll.Multiple(DiceType.D100, 3) == 3)
+    {
+        Console.WriteLine($"Found on {i}");
+        found = true;
+    }
+}
