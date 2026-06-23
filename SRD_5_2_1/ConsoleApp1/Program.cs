f65b836f2c5dd728d1ee68d40dc698f68489c367
Tranku <148291727+Tranku28@users.noreply.github.com>
2026-06-23 13:38:07 +0200

    Structure improvements

--- SRD_5_2_1/ConsoleApp1/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿
-using SRD_5_2_1.Domain;
-
-AbilityScores abilityScores = new(31, 4, 7, 10, 0, 1); 
---
+++ SRD_5_2_1/tools/CLI playground/Program.cs
@@ -0,0 +1,4 @@
+﻿
+using SRD_5_2_1.Domain;
+
+AbilityScores abilityScores = new(30, 4, 7, 10, 0, 1);
